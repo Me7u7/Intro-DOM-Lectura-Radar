310e907be2010120396f48a20d4dd21f7dcc8a2d
Akeri <samaescii@gmail.com>
2025-02-26 17:24:05 -0600

    Fecha y hora

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ function cuestionario() {
     let X = document.getElementById("X").value;
     let Op = document.getElementById("Op").value;
     let nickname = document.getElementById("nickname").value;
+    let fecha = new Date();
+    let fechaHora = fecha.toLocaleString();
     if(X && Op && nickname){
         let resumendiv = document.getElementById("resumen");
-        resumendiv.innerHTML = "<br>Título: "+X+"<br>El usuario "+nickname+" opina '"+Op+"'";
+        resumendiv.innerHTML = "<br> Su opinión fue resgistrada:  " + fechaHora+"<br>Título: "+X+"<br>El usuario "+nickname+" opina '"+Op;;
         alert("Gracias, tu opinión es muy valiosa");
         document.getElementById("nickname").value = "";
         document.getElementById("X").value = "";
