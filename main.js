62cd73fcd4f13c85e3113433175eaaf8eeb6a4cd
Akeri <samaescii@gmail.com>
2025-02-26 16:44:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+function cuestionario() {
+    let X = document.getElementById("X").value;
+    let Op = document.getElementById("Op").value;
+    let nickname = document.getElementById("nickname").value;
+    if(X && Op && nickname){
+        let resumendiv = document.getElementById("resumen");
+        resumendiv.innerHTML = "<br>Título: "+X+"<br>El usuario "+nickname+" opina '"+Op+"'";
+        alert("Gracias, tu opinión es muy valiosa");
+        document.getElementById("nickname").value = "";
+        document.getElementById("X").value = "";
+        document.getElementById("Op").value = "";
+    }else{
+        alert("Verifica que no haya campos vacíos");
+    }
+}
+
+
+
+
+
+
+
