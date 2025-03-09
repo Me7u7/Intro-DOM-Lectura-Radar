27a1a8e6976ef9bcc42585def5c84f54f65df9bb
Akeri <samaescii@gmail.com>
2025-03-09 17:38:10 -0600

    Registro de comentarios

--- main.js
+++ main.js
@@ -4,20 +4,30 @@ function cuestionario() {
     let nickname = document.getElementById("nickname").value;
     let fecha = new Date();
     let fechaHora = fecha.toLocaleString();
-    if(X && Op && nickname){
-        let resumendiv = document.getElementById("resumen");
-        resumendiv.innerHTML = "<br> Su opinión fue resgistrada:  " + fechaHora+"<br>Título: "+X+"<br>El usuario "+nickname+" opina '"+Op;;
-        alert("Gracias, tu opinión es muy valiosa");
+
+    if (X && Op && nickname) {
+//Creación de lista
+        let listaComentarios = document.getElementById("listaComentarios");
+
+//Opiniones
+        let comentarioDiv = document.createElement("div");
+        comentarioDiv.classList.add("comentario");
+
+        comentarioDiv.innerHTML = "<p>"+fechaHora+"</p><strong> USUARIO: </strong> "+nickname+"<br><strong> TÍTULO: </strong>"+X+"<br><strong>OPINIÓN: </strong>"+Op+"<br><br>";
+
+//Nuevo comentario
+        listaComentarios.appendChild(comentarioDiv);
+
+//Limpia las casillas de entrada
         document.getElementById("nickname").value = "";
         document.getElementById("X").value = "";
         document.getElementById("Op").value = "";
-    }else{
+
+        alert("Gracias, tu opinión es muy valiosa");
+    } else {
         alert("Verifica que no haya campos vacíos");
     }
 }
-
-
-
 
 
 
